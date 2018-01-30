17b79d4cc7e73e4e05870ee3c4f7008c0531c48e
Sandro Mani <manisandro@gmail.com>
2018-01-30 13:16:34 +0100

    Enable map info tooltip

--- js/appConfig.js
+++ js/appConfig.js
@@ -67,6 +67,7 @@ module.exports = {
             EditingPlugin: require('../qwc2/QWC2Components/plugins/Editing')(EditingInterface),
             MapComparePlugin: require('../qwc2/QWC2Components/plugins/MapCompare'),
             HeightProfilePlugin: require('../qwc2/QWC2Components/plugins/HeightProfile'),
+            MapInfoTooltipPlugin: require('../qwc2/QWC2Components/plugins/MapInfoTooltip'),
             AuthenticationPlugin: require('./plugins/Authentication')
         }
     },
