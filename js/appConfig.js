6f27378a806c8ef5804b34d264d13f94aeba6206
Sandro Mani <manisandro@gmail.com>
2018-06-21 16:32:31 +0200

    Use upstream authentication plugin

--- js/appConfig.js
+++ js/appConfig.js
@@ -69,7 +69,7 @@ module.exports = {
             MapComparePlugin: require('../qwc2/QWC2Components/plugins/MapCompare'),
             HeightProfilePlugin: require('../qwc2/QWC2Components/plugins/HeightProfile'),
             MapInfoTooltipPlugin: require('../qwc2/QWC2Components/plugins/MapInfoTooltip'),
-            AuthenticationPlugin: require('./plugins/Authentication'),
+            AuthenticationPlugin: require('../qwc2/QWC2Components/plugins/Authentication'),
             LandRegisterExtract: require('./plugins/LandRegisterExtract')
         }
     },
