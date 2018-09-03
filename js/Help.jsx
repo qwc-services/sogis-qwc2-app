6b4c7f37989f368035f6f4d0b4382c02f8e87216
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2018-09-03 16:58:41 +0200

    Changed Help.

--- js/Help.jsx
+++ js/Help.jsx
@@ -9,7 +9,28 @@
 const React = require('react');
 
 function renderHelp() {
-    return (<div><a href="https://www.so.ch/verwaltung/bau-und-justizdepartement/amt-fuer-geoinformation/hilfe/hilfe-web-gis-client/" target="_blank">Hilfe zum Web GIS Client</a></div>);
+    return (<div role="body">
+                <div style="margin: 0.5em;">
+                    <p>
+                        <img src="https://geo.so.ch/map/assets/img/logo.jpg" width="200">
+                    </p>
+                    <p>
+                        Daten: Kanton Solothurn / Gemeinden
+                        <br>
+                        Hintergrundkarten &copy; swisstopo / Kanton Solothurn
+                    </p>
+                    <p>
+                        <a href="https://www.so.ch/fileadmin/internet/bjd/bjd-agi/xls/Amtliche_Vermessung/Web_GIS_Client/Uebersicht_Karte.xlsx" target="_blank">Layerliste</a>
+                        <br>
+                        <a href="https://www.so.ch/verwaltung/bau-und-justizdepartement/amt-fuer-geoinformation/hilfe/hilfe-web-gis-client/" target="_blank">Benutzerhilfe</a>
+                        <br>
+                    </p>
+                    <p>
+                        <a href="https://geo.so.ch" target="_blank">Geoportal Kanton Solothurn</a>
+                        <br>Kontakt: agi@bd.so.ch
+                    </p>
+                 </div>
+             </div>);
 }
 
 module.exports = renderHelp;
