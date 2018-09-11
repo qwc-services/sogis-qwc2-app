6886039295813bf4a5b755e67368db2308589d66
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2018-09-11 08:52:09 +0200

    Change logo and layout help.

--- js/Help.jsx
+++ js/Help.jsx
@@ -12,18 +12,18 @@ function renderHelp() {
     return (<div role="body">
                 <div style={{margin: "0.5em"}}>
                     <p>
-                        <img src="https://geo.so.ch/map/assets/img/logo.jpg" width="200"/>
+                        <img src="https://geo.so.ch/map/assets/img/logo-transparent.png" width="200"/>
+                    </p>
+                    <p>
+                        <a href="https://www.so.ch/verwaltung/bau-und-justizdepartement/amt-fuer-geoinformation/hilfe/hilfe-web-gis-client/" target="_blank">Benutzerhilfe</a>
+                        <br/>
+                        <a href="https://www.so.ch/fileadmin/internet/bjd/bjd-agi/xls/Amtliche_Vermessung/Web_GIS_Client/Uebersicht_Karte.xlsx" target="_blank">Layerliste</a>
+                        <br/>
                     </p>
                     <p>
                         Daten: Kanton Solothurn / Gemeinden
                         <br/>
                         Hintergrundkarten &copy; swisstopo / Kanton Solothurn
-                    </p>
-                    <p>
-                        <a href="https://www.so.ch/fileadmin/internet/bjd/bjd-agi/xls/Amtliche_Vermessung/Web_GIS_Client/Uebersicht_Karte.xlsx" target="_blank">Layerliste</a>
-                        <br/>
-                        <a href="https://www.so.ch/verwaltung/bau-und-justizdepartement/amt-fuer-geoinformation/hilfe/hilfe-web-gis-client/" target="_blank">Benutzerhilfe</a>
-                        <br/>
                     </p>
                     <p>
                         <a href="https://geo.so.ch" target="_blank">Geoportal Kanton Solothurn</a>
