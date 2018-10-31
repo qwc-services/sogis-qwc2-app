d39991eb7931ec702eda1fe45f605839ff95da4f
Pirmin Kalberer <pka@sourcepole.ch>
2018-10-31 21:06:12 +0100

    Make artifact upload more generic

--- publishRelease.py
+++ publishRelease.py
@@ -4,48 +4,53 @@ import requests
 import json
 import sys
 
-if len(sys.argv) < 4:
-    sys.stderr.write("Usage: %s version tag token\n" % sys.argv[0])
+if len(sys.argv) < 5:
+    sys.stderr.write(
+        "Usage: %s project_id tag token filenames\n" % sys.argv[0])
     sys.exit(1)
 
-version = sys.argv[1]
+project_id = sys.argv[1]
 tag = sys.argv[2]
 token = sys.argv[3]
 
-apiurl = "https://git.sourcepole.ch/api/v4/projects/160"
-appbundle = "QWC2Solothurn-%s_appbundle.zip" % version
+apiurl = "https://git.sourcepole.ch/api/v4/projects/" + project_id
 
-# Upload file
-response = requests.post(
-    apiurl + "/uploads",
-    files={"file": open(appbundle, "rb")},
-    headers={'PRIVATE-TOKEN': token}
-)
+# Upload files
+upload_links = []
+for argno in range(4, len(sys.argv)):
+    fname = sys.argv[argno]
+    response = requests.post(
+        apiurl + "/uploads",
+        files={"file": open(fname, "rb")},
+        headers={'PRIVATE-TOKEN': token}
+    )
 
-try:
-    uploadurl = json.loads(response.text)["url"]
-except:
-    sys.stderr.write("Upload failed:\n%s\n" % response.text)
-    sys.exit(1)
-sys.stdout.write("File uploaded to %s\n" % uploadurl)
+    try:
+        uploadurl = json.loads(response.text)["url"]
+        upload_links.append("[%s](%s)" % (fname, uploadurl))
+    except:
+        sys.stderr.write("Upload failed:\n%s\n" % response.text)
+        sys.exit(1)
+    sys.stdout.write("File uploaded to %s\n" % uploadurl)
 
 # Create/Update release tag notes
 response = requests.post(
     apiurl + "/repository/tags/%s/release" % tag,
-    json={"description": "[%s](%s)" % (appbundle, uploadurl)},
+    json={"description": "%s" % ' '.join(upload_links)},
     headers={'PRIVATE-TOKEN': token}
 )
 if response.status_code == 409:
     # Already exists, update existing
     response = requests.put(
         apiurl + "/repository/tags/%s/release" % tag,
-        json={"description": "[%s](%s)" % (appbundle, uploadurl)},
+        json={"description": "%s" % ' '.join(upload_links)},
         headers={'PRIVATE-TOKEN': token}
     )
 
 try:
     data = json.loads(response.text)
-    sys.stdout.write("Published release %s: %s\n" % (data["tag_name"], data["description"]))
+    sys.stdout.write("Published release %s: %s\n" %
+                     (data["tag_name"], data["description"]))
 except:
     sys.stderr.write("Failed to create/update release:\n%s\n" % response.text)
     sys.exit(1)
