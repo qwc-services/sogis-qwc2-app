e54611700242191c6edcdbd3d18205fe1f3e55c7
Sandro Mani <manisandro@gmail.com>
2019-03-14 15:37:15 +0100

    Update submodule

--- js/themeLayerRestorer.js
+++ js/themeLayerRestorer.js
@@ -8,6 +8,7 @@
 
 const axios = require('axios');
 const ConfigUtils = require('../qwc2/MapStore2Components/utils/ConfigUtils');
+const LayerUtils = require('../qwc2/QWC2Components/utils/LayerUtils');
 
 function themeLayerRestorer(missingLayers, theme, callback) {
     // Invoked for layers specified in the l url parameter which are missing in the specified theme
@@ -15,10 +16,12 @@ function themeLayerRestorer(missingLayers, theme, callback) {
     let url = searchService.replace(/\/$/g, "") + "/getlayers";
     let params = {layers: missingLayers.join(",")};
     axios.get(url, {params: params}).then(response => {
-        let sublayers = response.data;
-        callback(Array.isArray(sublayers) ? sublayers : []);
+        let visibleLayers = Object.entries(response.data).reduce((res, [key, value]) => {
+            return {...res, [key]: LayerUtils.getSublayerNames({sublayers: value})};
+        }, {});
+        callback([].concat(...Object.values(response.data)), visibleLayers);
     }).catch(e => {
-        callback([]);
+        callback([], null);
     });
 }
 
