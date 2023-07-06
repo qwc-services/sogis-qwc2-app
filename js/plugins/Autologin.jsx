ab00c8adbedc8dd48c348f539945a777a7731020
Sandro Mani <manisandro@gmail.com>
2023-07-06 12:56:22 +0200

    Merge Autologin changes from somap_2_0 branch

--- js/plugins/Autologin.jsx
+++ js/plugins/Autologin.jsx
@@ -15,6 +15,7 @@ import url from 'url';
 import ConfigUtils from 'qwc2/utils/ConfigUtils';
 import {UrlParams} from 'qwc2/utils/PermaLinkUtils';
 
+
 class Autologin extends React.Component {
     static propTypes = {
         autologinUrl: PropTypes.string,
@@ -38,36 +39,58 @@ class Autologin extends React.Component {
             return;
         }
         this.initialized = true;
+        // Autologin flow:
+        // GET /map/
+        // Autologin.jsx:
+        //   if param config:autologin is undefined:
+        //     if GET /auth/info has no identity:
+        //       if in_net is True (intranet network match):
+        //         Redirect to /auth/login?url=/map/?config:autologin=1
+        // /auth/acs:
+        //   Write username + autologin into JWT cookie
+        //   Redirect to /map/?config:autologin=1
+        // StandardApp.jsx:
+        //   if param config:xxx passed:
+        //      load config.json with param xxx
+        //      remove config:xxx param from URL
+        // qwc2_viewer.qwc2_config:
+        //   if param config:autologin passed or autologin in identity:
+        //      remove login/logout menu entries
+        //
+        // Manual login flow:
+        // ...
+        //         Redirect to /auth/login?url=/map/
+        // /auth/acs:
+        //   Write username (without autologin) into JWT cookie
+        //   Redirect to /map/
+
         // No action, if redirected from auth service
         if (UrlParams.getParam('config:autologin') !== undefined) {
             UrlParams.updateParams({"config:autologin": undefined});
             return;
         }
 
-        const authServiceUrl = ConfigUtils.getConfigProp('authServiceUrl');
+        let authServiceUrl = ConfigUtils.getConfigProp('authServiceUrl');
 
         axios.get(authServiceUrl + '/info').then(res => {
-            if (!res.data.username) {
-                axios.head(this.props.autologinUrl, {
-                    // we don't need a real fetch
-                    // just checking whether Intranet URL resolves
-                    mode: 'no-cors'
-                }).then(() => {
+            if(!res.data.username) {
+                if (res.data.in_net) {
                     // automatic login
-                    const urlObj = url.parse(window.location.href);
+                    let urlObj = url.parse(window.location.href);
                     urlObj.query = {...this.props.startupParams};
                     urlObj.query["config:autologin"] = 1;
                     urlObj.search = undefined;
                     window.location.href = authServiceUrl + "login?url=" + encodeURIComponent(url.format(urlObj));
-                }).catch(() => {});
+                }
             }
-        }).catch(() => {});
+        }).catch(e => {});
     }
     render() {
         return null;
     }
-}
+};
 
 export default connect((state) => ({
     startupParams: state.localConfig.startupParams
 }), {})(Autologin);
+
