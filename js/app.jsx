8a972cb7210c64bdbecb9e0a890728fc109ec8cd
Sandro Mani <manisandro@gmail.com>
2024-07-10 22:30:41 +0200

    Inject tenant header if started config:tenant present passed in startup url

--- js/app.jsx
+++ js/app.jsx
@@ -64,6 +64,17 @@ if (UrlParams.getParam('config:autologin') !== undefined) {
 }
 
 function renderApp() {
+
+    if (UrlParams.getParam('config:tenant') !== undefined) {
+        // Add tenant header request interceptor
+        axios.interceptors.request.use((reqconfig) => {
+            const pair = UrlParams.getParam('config:tenant').split("=");
+            UrlParams.updateParams({'config:tenant': undefined});
+            reqconfig.headers[pair[0]] =  pair[1];
+            return reqconfig;
+        });
+    }
+
     const container = document.getElementById('container');
     const root = createRoot(container);
     root.render(<StandardApp appConfig={appConfig}/>);
